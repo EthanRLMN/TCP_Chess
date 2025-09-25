229c22139eb1615cba68408db783aebcb6079d56
Ethan Ruhlmann <e.ruhlmann@student.isartdigital.com>
2025-09-25 12:35:21 +0200

    [ADD] Test for client connection

---
+++ Assets/Scripts/PlayerSystem.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+
+public class PlayerSystem : MonoBehaviour
+{
+    [SerializeField] private string m_playerName = "Player1";
+    [SerializeField] private bool m_isOnline = false;
+    [SerializeField] private bool m_isPlaying = true;
+    
+    private Client m_client;
+
+
+    private void Awake()
+    {
+        m_client = FindFirstObjectByType<Client>();
+    }
+
+
+    private void LateUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            m_client.ConnectAttempt();
+        }
+        
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            m_client.Disconnect();
+        }
+    }
+}
